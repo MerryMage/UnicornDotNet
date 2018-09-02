7e93d1032c69efaf60dc802a5f7069d417ce69bc
MerryMage <MerryMage@users.noreply.github.com>
2018-09-02 21:39:25 +0100

    undef NDEBUG

--- qemu/include/qemu/osdep.h
+++ qemu/include/qemu/osdep.h
@@ -24,6 +24,7 @@
  * This work is licensed under the terms of the GNU GPL, version 2 or later.
  * See the COPYING file in the top-level directory.
  */
+#undef NDEBUG
 #ifndef QEMU_OSDEP_H
 #define QEMU_OSDEP_H
 
